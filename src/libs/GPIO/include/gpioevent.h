86f3cd8584c2d4ac36d71ffdc8f24e8d1df46f0b
Renn8 <115508374+Renn8@users.noreply.github.com>
2025-04-15 17:23:35 +0100

    Update gpioevent.h
    Typo was really fixed this time

--- src/libs/GPIO/include/gpioevent.h
+++ src/libs/GPIO/include/gpioevent.h
@@ -66,7 +66,7 @@ namespace GPIOName {
         void WorkerDataCollect();
 
         /**
-         * @brief Changes a boolaen to end the worker
+         * @brief Changes a boolean to end the worker
          */
         void GPIOStop();
 
