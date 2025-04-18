a4e759d7cb747443e38c381de7e04e560f097746
calumr2308 <calumr999@gmail.com>
2025-04-18 13:49:35 +0100

    Cant' see commit?

--- tests/CTestTestfile.cmake
+++ tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(Test_all "/home/SnairBeat/Real_Time/SnAIRbeat/run_tests")
-set_tests_properties(Test_all PROPERTIES  WORKING_DIRECTORY "/home/SnairBeat/Real_Time/SnAIRbeat" _BACKTRACE_TRIPLES "/home/SnairBeat/Real_Time/SnAIRbeat/tests/CMakeLists.txt;25;add_test;/home/SnairBeat/Real_Time/SnAIRbeat/tests/CMakeLists.txt;0;")
+set_tests_properties(Test_all PROPERTIES  WORKING_DIRECTORY "/home/SnairBeat/Real_Time/SnAIRbeat" _BACKTRACE_TRIPLES "/home/SnairBeat/Real_Time/SnAIRbeat/tests/CMakeLists.txt;24;add_test;/home/SnairBeat/Real_Time/SnAIRbeat/tests/CMakeLists.txt;0;")
